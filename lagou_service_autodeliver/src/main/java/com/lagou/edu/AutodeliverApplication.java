b757a5da126e909aa52a0bde36a98f2ff2623a23
zhangchi <773370688@qq.com>
2020-11-09 11:33:34 +0800

    将简历微服务和投递微服务加入注册中心

--- lagou_service_autodeliver/src/main/java/com/lagou/edu/AutodeliverApplication.java
+++ lagou_service_autodeliver/src/main/java/com/lagou/edu/AutodeliverApplication.java
@@ -2,6 +2,7 @@ package com.lagou.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
  * @create 2020-11-08
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AutodeliverApplication {
     public static void main(String[] args) {
         SpringApplication.run(AutodeliverApplication.class,args);
--- lagou_service_autodeliver/src/main/java/com/lagou/edu/controller/AutodeliverController.java
+++ lagou_service_autodeliver/src/main/java/com/lagou/edu/controller/AutodeliverController.java
@@ -1,11 +1,17 @@
 package com.lagou.edu.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author zhangchi
@@ -18,6 +24,15 @@ public class AutodeliverController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+
+    /**
+     * RestTemplate 方式
+     * @param userId
+     * @return
+     */
     @GetMapping("checkstate/{userId}")
     public Integer findResumeOpenState(@PathVariable Long userId){
         String url = "http://localhost:8080/resume/openState/";
@@ -26,5 +41,25 @@ public class AutodeliverController {
 
         return state;
     }
+
+
+    @GetMapping("checkAndBegin/{userId}")
+    public Integer findResumeOpenStates(@PathVariable Long userId){
+       // String url = "http://localhost:8080/resume/openState/";
+
+        //根据服务名称获取服务实例信息
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("LAGOU_SERVICE_RESUME");
+        //取出第一个实例 不考虑负载
+        ServiceInstance serviceInstance = instanceList.get(0);
+        String host = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+        //拼接远程地址
+        String url = "http://" + host + ":" + port + "/resume/openState/" + userId;
+
+        Integer state = restTemplate.getForObject(url, Integer.class);
+
+        return state;
+    }
+
 
 }
